f01704a35078955dab789aea441bc321f9e5b40e
ergemp <ergemp@gmail.com>
2022-08-09 14:18:30 +0300

    offset commit examples added

--- src/main/java/introduction/readSemantics/OffsetCommitExamples/OffsetCommitWithExceptionHandling.java
+++ src/main/java/introduction/readSemantics/OffsetCommitExamples/OffsetCommitWithExceptionHandling.java
@@ -1,4 +1,4 @@
-package introduction.readSemantics.ExceptionHandling;
+package introduction.readSemantics.OffsetCommitExamples;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
--- src/main/java/introduction/readSemantics/OffsetCommitExamples/OffsetCommitWithManual.java
+++ src/main/java/introduction/readSemantics/OffsetCommitExamples/OffsetCommitWithManual.java
@@ -1,4 +1,4 @@
-package introduction.readSemantics.ExceptionHandling;
+package introduction.readSemantics.OffsetCommitExamples;
 
 import org.apache.kafka.clients.consumer.*;
 import org.apache.kafka.common.TopicPartition;
