b7c8cbea46a627a30876e2206ab2eaf803c04032
ergemp <ergemp@gmail.com>
2022-08-12 15:20:43 +0300

    consumer assignment strategy example added

--- src/main/java/advanced/ConsumerPartitionAssignmentStrategy.java
+++ src/main/java/advanced/ConsumerPartitionAssignmentStrategy.java
@@ -1,5 +1,6 @@
-package introduction.consumer;
+package advanced;
 
+import introduction.consumer.ConsumerRebalancer;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
@@ -12,21 +13,24 @@ import java.util.Properties;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ConsumerBasic {
+public class ConsumerPartitionAssignmentStrategy {
 
-    private static KafkaConsumer<String, String> consumer;
+    private static final Logger log = LoggerFactory.getLogger(ConsumerRebalancer.class.getSimpleName());
 
     public static void main(String[] args) {
-
         Properties props = new Properties();
-        props.put("bootstrap.servers", "localhost:9092");
-        props.put("group.id", "ConsumerBasic");
-        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "ConsumerBasic");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
 
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
 
-        consumer = new KafkaConsumer<String, String>(props);
+        //props.put(ConsumerConfig.PARTITION_ASSIGNMENT_STRATEGY_CONFIG, "org.apache.kafka.clients.consumer.CooperativeStickyAssignor");
+
+
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(props);
+
         consumer.subscribe(Collections.singletonList("mytopic"));
 
         while (true) {
@@ -37,5 +41,6 @@ public class ConsumerBasic {
                         record.topic(), record.partition(), record.offset(), record.key(), record.value()));
             }
         }
+
     }
 }
