522f9588dca4ef7775a0128e0863ebb0fdc11f57
ergemp <ergemp@gmail.com>
2024-07-08 22:32:23 -0700

    security examples added

---
+++ src/main/java/advanced/security/SaslScramConsumerExample.java
@@ -0,0 +1,56 @@
+package advanced.security;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+
+public class SaslScramConsumerExample {
+
+    private static KafkaConsumer<String, String> consumer;
+
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9093,localhost:9094");
+        props.put("group.id", "SaslScramConsumerExample");
+        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+
+        props.put("security.protocol","SASL_PLAINTEXT");
+        props.put("sasl.jaas.config","org.apache.kafka.common.security.scram.ScramLoginModule required username=\"ergemp\" password=\"123456\";");
+        props.put("sasl.mechanism","SCRAM-SHA-256");
+
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        consumer = new KafkaConsumer<String, String>(props);
+        consumer.subscribe(Collections.singletonList("mytopic"));
+
+        try {
+            while (true) {
+                ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+
+                for (ConsumerRecord<String, String> record : records) {
+                    System.out.println(String.format("topic = %s, partition = %s, offset = %d, key = %s, value = %s\n",
+                            record.topic(), record.partition(), record.offset(), record.key(), record.value()));
+                }
+                consumer.commitAsync();
+            }
+        }
+        catch(Exception ex) {
+            ex.printStackTrace();
+        }
+        finally {
+            try {
+                consumer.commitSync();
+            } finally {
+                consumer.close();
+            }
+        }
+
+    }
+}
---
+++ src/main/java/advanced/security/SaslScramProducerExample.java
@@ -0,0 +1,39 @@
+package advanced.security;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+public class SaslScramProducerExample {
+
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        //props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9093,localhost:9094");
+        props.put(ProducerConfig.CLIENT_ID_CONFIG, "SaslScramProducerExample");  //client.id
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());  //key.serializer
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());  //value.serializer
+
+        props.put("security.protocol","SASL_PLAINTEXT");
+        props.put("sasl.jaas.config","org.apache.kafka.common.security.scram.ScramLoginModule required username=\"ergemp\" password=\"123456\";");
+        props.put("sasl.mechanism","SCRAM-SHA-256");
+
+        //create the producer
+        Producer producer = new KafkaProducer(props);
+
+        //create the record
+        ProducerRecord<String, String> record = new ProducerRecord<>("mytopic", "test message from java-1");
+
+        try {
+            producer.send(record);
+            producer.flush();
+            producer.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
